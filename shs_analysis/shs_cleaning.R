3405a9cd0de1b4cebb7bf0a46e3d3d3fc023b9a4
Matthew Lowes <matt.lowes@oneacrefund.org>
2018-10-01 12:04:51 +0300

    updated to cc export instead of api. api was creating issues

--- shs_analysis/shs_cleaning.R
+++ shs_analysis/shs_cleaning.R
@@ -18,14 +18,29 @@ cat("\014")
 libs <- c("tidyverse", "knitr", "readxl")
 lapply(libs, require, character.only = T)
 
-
+select <- dplyr::select
 
 #### load data ####
 # load the ke r1 data to combine the new data with
-load("../ke_round_1/ke_r1_cleaned_combined.Rdata")
+load("../ke_round_1/ke_r1_cleaned_combined.Rdata") # fieldDat
 
 # and the latest kenya survey rounds, should be 2 and 3??
+source("../../oaflib/commcareExport.R")
+source("../../oaflib/misc.R")
 
-#### prep data ####
+
+# load the latest Kenya data to add to the existing data
+keDat17 <- read.csv("Kenya_2017_soil_health_study_data.csv")
+
+# update names to the fieldDat names
+names(fieldDat)
+
+
+thingsToRemove <- paste(c("form\\.consent_yes\\.", "demography\\.", "basic_information\\.", "field\\.", "plot_information\\.", "inputs\\.", "other_respondent_details\\.", "field_information\\.", "livestock\\."), collapse = "|")
+names(keDat17) <- gsub(thingsToRemove, "", names(keDat17))
+names(keDat17) <- tolower(names(keDat17))
+
+# and now figure out how to match variable names as quickly as possible. 
+# I'll first weed out any variable names that already match and only focus on the remaining ones
 
 
